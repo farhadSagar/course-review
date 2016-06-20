e68b10d330d00216b8b6c05f494af18e94f20a32
Farhad Ahmed Sagar <farhad.cfc@gmail.com>
2016-06-20 17:41:41 -0400

    testing for heroku

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+/*var mongoose = require('mongoose');
 
 //Setup Mongo COnnection
 mongoose.connect('mongodb://localhost/coursereview');
-
+*/
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
